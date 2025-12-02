67e7186a4f244e59f0d4a5f781ed76bd41832eb6
Balram Meena <bmeena97562@gmail.com>
2025-12-02 17:45:17 +0530

    some practice

--- DSA/Introduction To C++/Day1/first.cpp
+++ DSA/Introduction To C++/Day1/first.cpp
@@ -1,12 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int a = 10;
     bool adult = true;
-    float b=4.3;
-    char c='d';
-    cout<<sizeof(a)<<":"<<sizeof(adult)<<":"<<sizeof(b)<<":"<<sizeof(c);
-    cout<<"\n"<<sizeof("hello world");
+    float b = 4.3;
+    char c = 'd';
+    // cout<<sizeof(a)<<":"<<sizeof(adult)<<":"<<sizeof(b)<<":"<<sizeof(c);
+    // cout<<"\n"<<sizeof("hello world");
+    string str = "John Kite";
+    // cout << str.length();
+    string firstName = "Rohit";
+    string lastName = "Sharma";
+    string fullName = firstName + " " + lastName;
+    // cout<<fullName<<endl;
+
+    string input;
+    getline(cin,input);
+    cout<<input;
+
     return 0;
 }
