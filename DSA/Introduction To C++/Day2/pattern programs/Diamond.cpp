1453748381200a85c75d881cde907358f246ea16
Balram Meena <bmeena97562@gmail.com>
2025-12-04 05:37:47 +0530

    do some pattern questions

--- DSA/Introduction To C++/Day2/pattern programs/Diamond.cpp
+++ DSA/Introduction To C++/Day2/pattern programs/Diamond.cpp
@@ -3,7 +3,32 @@ using namespace std;
 
 int main()
 {
-    // Diamond Pattern
+
+    for (int i = 1; i <=5; i++)
+    {
+
+        for (int j = 1; j <= 5 - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 4; i > 0; i--)
+    {
+        for (int j = 1; j <= 5 - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
