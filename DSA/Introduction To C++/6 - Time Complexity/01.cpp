b31609917e4af076fd146447c715f75bfea79a6d
Balram Meena <bmeena97562@gmail.com>
2026-01-09 12:01:43 +0530

    commit

---
+++ DSA/Introduction To C++/6 - Time Complexity/01.cpp
@@ -0,0 +1,74 @@
+
+// Programs
+
+// for(int i=0;i<n;i++){
+//     cout<<"Hello";
+// }
+
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<n;j++){
+//         cout<<"Hello";
+//     }
+// }
+
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<=i;j++){
+//         cout<<"Hello";
+//     }
+// }
+
+
+// for(int i=0;i<n;i=i+2){
+//     cout<<"Hello";
+// }
+
+
+// for(int i=1;i<n;i=i*2){
+//     cout<<"Hello";
+// }
+
+// for(int i=1;i<n;i=i*3){
+//     cout<<"Hello";
+// }
+
+// for(int i=n;i>1;i=i/2){
+//     cout<<"Hello";
+// }
+
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<n;j++){
+//         for(int k=0;k<n;k++){
+//             cout<<"Hello";
+//         }
+//     }
+// }
+
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<i*i;j++){
+//         cout<<"Hello";
+//     }
+// }
+
+
+
+
+
+// for(int i=0;i<n;i++){
+//    for(int j=0;j<n;j++){
+//     cout<<"Hello";
+//    }
+// }
+
+
+// for(int i=0;i<n;i++){
+   
+//     //  n operation
+// }
+
+
+// for(int i=0;i<n;i++){
+//     for(j=0;j<=i;j++){
+//         cout<<"Hello";
+//     }
+// }
+
