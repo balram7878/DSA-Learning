ec50aa1e1cdec23a26dca33ae555cb9d5161720f
Balram Meena <bmeena97562@gmail.com>
2026-01-10 12:12:52 +0530

    asymptotic notations

--- DSA/Introduction To C++/6 - Time Complexity/01.cpp
+++ DSA/Introduction To C++/6 - Time Complexity/01.cpp
@@ -1,41 +1,41 @@
 
 // Programs
 
-// for(int i=0;i<n;i++){
+// for(int i=0;i<n;i++){    --> time complexity = n
 //     cout<<"Hello";
 // }
 
-// for(int i=0;i<n;i++){
+// for(int i=0;i<n;i++){      --> time complexity = n^2
 //     for(int j=0;j<n;j++){
 //         cout<<"Hello";
 //     }
 // }
 
-// for(int i=0;i<n;i++){
+// for(int i=0;i<n;i++){     --> time complexity = n^2
 //     for(int j=0;j<=i;j++){
 //         cout<<"Hello";
 //     }
 // }
 
 
-// for(int i=0;i<n;i=i+2){
+// for(int i=0;i<n;i=i+2){    --> time complexity = n because of n/2 iterations
 //     cout<<"Hello";
 // }
 
 
-// for(int i=1;i<n;i=i*2){
+// for(int i=1;i<n;i=i*2){   --> time complexity = log2(n)
 //     cout<<"Hello";
 // }
 
-// for(int i=1;i<n;i=i*3){
+// for(int i=1;i<n;i=i*3){   ---> time complexity = log3(n)
+//     cout<<"Hello"; 
+// }
+
+// for(int i=n;i>1;i=i/2){   --> time complexity = log2(n)
 //     cout<<"Hello";
 // }
 
-// for(int i=n;i>1;i=i/2){
-//     cout<<"Hello";
-// }
-
-// for(int i=0;i<n;i++){
+// for(int i=0;i<n;i++){    --> time complexity = n^3
 //     for(int j=0;j<n;j++){
 //         for(int k=0;k<n;k++){
 //             cout<<"Hello";
@@ -43,31 +43,8 @@
 //     }
 // }
 
-// for(int i=0;i<n;i++){
+// for(int i=0;i<n;i++){  ---> time complexity = n^3
 //     for(int j=0;j<i*i;j++){
-//         cout<<"Hello";
-//     }
-// }
-
-
-
-
-
-// for(int i=0;i<n;i++){
-//    for(int j=0;j<n;j++){
-//     cout<<"Hello";
-//    }
-// }
-
-
-// for(int i=0;i<n;i++){
-   
-//     //  n operation
-// }
-
-
-// for(int i=0;i<n;i++){
-//     for(j=0;j<=i;j++){
 //         cout<<"Hello";
 //     }
 // }
