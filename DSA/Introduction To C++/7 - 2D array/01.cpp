0f1ed15306f4b047b260f6abb552cf64fd27ee6f
Balram Meena <bmeena97562@gmail.com>
2026-01-26 22:05:53 +0530

    some code

--- DSA/Introduction To C++/7 - 2D array/01.cpp
+++ DSA/Introduction To C++/7 - 2D array/01.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
+#include<string>
 using namespace std;
+void toLower(string &s) {
+    // Your code here
+	string result="";
+    // cout<<"Original string: "<<s<<endl;
+    // cout<<"string length: "<<s.length()<<endl;
+	for(int i=0; i<s.length(); i++){
+        cout<<int(s[i])<<endl;
+		if(int(s[i])>=65 && int(s[i])<=90){
+		result[i]=(int(int(s[i])+32));
+        cout<<result;
+        }
+		else
+		result[i]=s[i];
+        cout<<result;
+	}
+	cout<<result;
+
+}
 int main(){
-    
+    cout<<"Convert string to lowercase"<<endl;
+    // cout<<int('Z')<<endl;
+    string s="Hello";
+    toLower(s);
     return 0;
 }
